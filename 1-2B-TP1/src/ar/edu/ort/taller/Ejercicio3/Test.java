bd35f17c3e00c46f2751558e81eed928d8b6f2d3
David S Gonzalez <davidsghz@gmail.com>
2018-08-12 20:11:19 -0300

    Contiene 2 carreras. En la primera existe un unico ganador, en la segunda existen 2 ganadores

--- 1-2B-TP1/src/ar/edu/ort/taller/Ejercicio3/Test.java
+++ 1-2B-TP1/src/ar/edu/ort/taller/Ejercicio3/Test.java
@@ -3,7 +3,74 @@ package ar.edu.ort.taller.Ejercicio3;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Creo una nueeva competencia
+		Competencia carrera1 = new Competencia("100 mts");
+		
+		// Creo atletas
+		Atleta atleta1 = new Atleta("Antonio", 9.81);
+		Atleta atleta2 = new Atleta("Bruno", 9.85);
+		Atleta atleta3 = new Atleta("Carlos", 9.80);
+		Atleta atleta4 = new Atleta("Daniel", 10.01);
+		Atleta atleta5 = new Atleta("Esteban", 9.97);
+		
+		// Agrego los atletas a la competencia
+		carrera1.agregarParticipantes(atleta1);
+		carrera1.agregarParticipantes(atleta2);
+		carrera1.agregarParticipantes(atleta3);
+		carrera1.agregarParticipantes(atleta4);
+		carrera1.agregarParticipantes(atleta5);
+		
+		System.out.println("Competencia #1");
+		System.out.println();
+		System.out.println("Participantes");
+		//Muestro los atletas que participan en la competencia
+		for (Atleta participantes : carrera1.getAtletasConcursantes()) {
+			System.out.println(participantes.toString());
+		}
+		
+		System.out.println();
+		
+		System.out.println("Atletas ganadores: ");
+		for (Atleta ganadores : carrera1.getGanadores()) {
+			System.out.println(ganadores.toString());
+		}
+		System.out.println();
+		System.out.println("  ********************************************  ");
+		
+		/* Competencia 2 */
+		System.out.println("Competencia #2");
+		System.out.println();
+		System.out.println("Participantes");
+		
+		// Creo una nueeva competencia
+		Competencia carrera2 = new Competencia("100 mts");
+
+		// Creo atletas
+		Atleta atleta6 = new Atleta("Francisco", 9.79);
+		Atleta atleta7 = new Atleta("Gabriel", 9.80);
+		Atleta atleta8 = new Atleta("Hector", 9.79);
+		Atleta atleta9 = new Atleta("Ignacio", 9.81);
+		Atleta atleta10 = new Atleta("Javier", 9.85);
+
+		// Agrego los atletas a la competencia
+		carrera2.agregarParticipantes(atleta6);
+		carrera2.agregarParticipantes(atleta7);
+		carrera2.agregarParticipantes(atleta8);
+		carrera2.agregarParticipantes(atleta9);
+		carrera2.agregarParticipantes(atleta10);
+
+		//Muestro los atletas que participan en la competencia
+		for (Atleta participantes : carrera2.getAtletasConcursantes()) {
+			System.out.println(participantes.toString());
+		}
+		System.out.println();
+		System.out.println("  ********************************************  ");
+
+		System.out.println("Atletas ganadores: ");
+		for (Atleta ganadores : carrera2.getGanadores()) {
+			System.out.println(ganadores.toString());
+		}
 
 	}
 
