edc39b994c69e8566477c7db916919cc7b0a37bb
dsgonzalez <dsgonzalez@practia.global>
2018-08-13 20:45:32 -0300

    Verificacion del metodo diasTrnascurridos en la clase Test

--- 1-2B-TP1/src/ar/edu/ort/taller/Ejercicio4/Test.java
+++ 1-2B-TP1/src/ar/edu/ort/taller/Ejercicio4/Test.java
@@ -3,7 +3,13 @@ package ar.edu.ort.taller.Ejercicio4;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Anio a1 = new Anio();
+		
+		System.out.println("Cantidad de dias transcurridos en todo el año");
+		System.out.println("Han transcurrido: "+a1.diasTranscurridos(1)+" dias");
+		
+		
 
 	}
 
