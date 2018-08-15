a4ebb2fe1ade47a8e706b9c4b5c997a429dcbf09
dsgonzalez <dsgonzalez@practia.global>
2018-08-14 22:40:44 -0300

    Pequeños test para validar los metodos

--- 1-2B-TP1/src/ar/edu/ort/taller/Ejercicio5/Test.java
+++ 1-2B-TP1/src/ar/edu/ort/taller/Ejercicio5/Test.java
@@ -1,9 +1,21 @@
 package ar.edu.ort.taller.Ejercicio5;
 
+import ar.edu.ort.taller.Ejercicio5.AnioV2;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		AnioV2 a2 = new AnioV2();
+		
+		System.out.println("Listado de Meses");
+		for (int i = 0; i < a2.getMeses().length; i++) {
+			System.out.println(a2.getMeses()[i]);
+		}
+		
+		System.out.println();
+		System.out.println("Obtener Nombre de un mes:");
+		System.out.println(a2.getNombreMes(7));
 
 	}
 
