21f6ae0b265db76dc30b9afc446a4056ab6e4a24
David S Gonzalez <davidsghz@gmail.com>
2018-08-19 09:18:36 -0300

    Pruebas de verificacion

--- 1-2B-TP1/src/ar/edu/ort/taller/Ejercicio6/Test.java
+++ 1-2B-TP1/src/ar/edu/ort/taller/Ejercicio6/Test.java
@@ -3,7 +3,36 @@ package ar.edu.ort.taller.Ejercicio6;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		GastoAnual gastoAnual = new GastoAnual();
+		gastoAnual.agregarGasto(MES.ENERO, "administracion", 1500);
+		gastoAnual.agregarGasto(MES.ENERO, "tecnico", 150);
+		gastoAnual.agregarGasto(MES.FEBRERO, "tecnico", 500);
+		gastoAnual.agregarGasto(MES.MARZO, "comercial", 750);
+		gastoAnual.agregarGasto(MES.ABRIL, "funcional", 200);
+		gastoAnual.agregarGasto(MES.MAYO, "comercial", 7500);
+		gastoAnual.agregarGasto(MES.JUNIO, "administracion", 500);
+		gastoAnual.agregarGasto(MES.JULIO, "funcional", 50);
+		gastoAnual.agregarGasto(MES.AGOSTO, "funcional", 300);
+		gastoAnual.agregarGasto(MES.SEPTIEMBRE, "tecnico", 500);
+		gastoAnual.agregarGasto(MES.OCTUBRE, "comercial", 300);
+		gastoAnual.agregarGasto(MES.NOVIEMBRE, "administracion", 1000);
+		gastoAnual.agregarGasto(MES.DICIEMBRE, "comercial", 400);
+		
+		//System.out.println(gastoAnual.gastoAcumulado(MES.FEBRERO));
+		//System.out.println();
+		
+		System.out.println("Gasto del rubro tecnico: "+gastoAnual.gastoAcumulado("tecnico"));
+		System.out.println("Gasto del rubro administracion: "+gastoAnual.gastoAcumulado("administracion"));
+		System.out.println("Gasto del rubro comercial: "+gastoAnual.gastoAcumulado("comercial"));
+		System.out.println("Gasto del rubro funcional: "+gastoAnual.gastoAcumulado("funcional"));
+		System.out.println("Gasto del rubro zarlanga: "+gastoAnual.gastoAcumulado("zarlanga"));
+		
+		System.out.println();
+		gastoAnual.informarConsumosPorMes();
+		
+		System.out.println();
+		gastoAnual.informarPromedioMensualPorRubro();
 
 	}
 
