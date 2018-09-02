560cbdfde40bba6b18fdbeb6059d09d88ebbbd88
topetin <mariana.carballal@hotmail.com>
2018-09-01 23:48:57 -0300

    Ejercicio 4. Revisado y corregido en Test

--- 1-2B-TP1/src/ar/edu/ort/taller/Ejercicio4/Test.java
+++ 1-2B-TP1/src/ar/edu/ort/taller/Ejercicio4/Test.java
@@ -6,29 +6,36 @@ public class Test {
 		
 		Anio a1 = new Anio();
 		
-		System.out.println("Listado de meses en el año");
+		//Imprimir listado de meses del anio
 		for (String meses : a1.getMeses()) {
 			System.out.println(meses);
 		}
+		
+		//Imprimir cantidad de dias transcurridos en el anio
 		System.out.println();
-		System.out.println("Cantidad de dias transcurridos en todo el año");
 		printDiasTranscurridos(a1, 1);
+		
+		//Imprimir cantidad de dias transcurridos hasta el mes 5
 		System.out.println();
-		System.out.println("Cantidad de dias transcurridos hasta el mes 5");
 		printDiasTranscurridos(a1, 5);
+		
+		//Cumpleanios Guillermo
 		System.out.println();
 		System.out.println("Guillermo nacio en el mes 2");
 		printDiasTranscurridos(a1, 2);
 		printMesNaciomiento(a1,2);
+		
+		//Cumpleanios David
 		System.out.println();
 		System.out.println("David nacio en el mes 1");
 		printDiasTranscurridos(a1, 1);
 		printMesNaciomiento(a1,1);
+		
+		//Cumpleanios Mariana
 		System.out.println();
-		System.out.println("Mariana nacio en el mes 9");
-		printDiasTranscurridos(a1, 9);
-		printMesNaciomiento(a1,9);
-
+		System.out.println("Mariana nacio en el mes 4");
+		printDiasTranscurridos(a1, 4);
+		printMesNaciomiento(a1,4);
 	}
 	
 	private static void printMesNaciomiento(Anio a, int mes) {
