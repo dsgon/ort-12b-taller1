f8bb845f6b992db8d3321ff9b8ab51a662b96eb2
dsgon <davidsghz@gmail.com>
2018-09-03 21:40:02 -0300

    Creacion de primera parte

--- 1-2B-TP2/src/ar/edu/ort/taller1/tp2/Ejercicio3/Test.java
+++ 1-2B-TP2/src/ar/edu/ort/taller1/tp2/Ejercicio3/Test.java
@@ -1,10 +1,20 @@
 package ar.edu.ort.taller1.tp2.Ejercicio3;
 
+import java.util.ArrayList;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		ArrayList<Electrodomestico> inventario = new ArrayList<Electrodomestico>();
+		
+		Electrodomestico heladera1 = new Heladera("Whirlpool",
+				"H2745", "WH2745-0001", "220", "apagado", 14999,
+				"250", "no frost");
+		Electrodomestico tv1 = new Televisor("Philips", "49PGFS",
+				"PTV49GFS-0001", "220", "apagado", 14370, "49", "Smart");
+		
+		
 	}
 
 }
