0284ea668b629047a9ca88daf57f6220640d6cf9
Balnir <antun.guillermo@gmail.com>
2018-09-04 22:05:18 -0300

    Ejercicio1: Modelado de clases
    Este commit contiene todo el modelado de clases de todo el ejercicio 1 del TP 2

--- 1-2B-TP2/src/ar/edu/ort/taller1/tp2/Ejercicio1/Asalariado.java
+++ 1-2B-TP2/src/ar/edu/ort/taller1/tp2/Ejercicio1/Asalariado.java
@@ -1,5 +1,19 @@
 package ar.edu.ort.taller1.tp2.Ejercicio1;
 
-public class Asalariado {
-
+public class Asalariado extends Empleado {
+	
+	private float sueldo;
+	
+	public Asalariado(String nombre, int edad,float sueldo) {
+		super(nombre,edad);
+		this.sueldo = sueldo;
+	}
+	@Override
+	public String toString() {
+		return "[Nombre=" + super.getNombre() + ", edad=" + super.getEdad() + " salario: " + this.sueldo + "]";
+	}	
+	public float calcularPago() {
+		return this.sueldo;
+	}
+	
 }
--- 1-2B-TP2/src/ar/edu/ort/taller1/tp2/Ejercicio1/SubContratado.java
+++ 1-2B-TP2/src/ar/edu/ort/taller1/tp2/Ejercicio1/SubContratado.java
@@ -1,5 +1,27 @@
 package ar.edu.ort.taller1.tp2.Ejercicio1;
 
-public class SubContratado {
+public class SubContratado extends Empleado {
+	private float cantHoras;
+	private float precioHora;
+	
+	public SubContratado() {
+		super();
+	}
+	
+	public SubContratado (String Nombre, int edad, float cantHoras, float precioHora) {
+		super(Nombre, edad);
+		this.cantHoras = cantHoras;
+		this.precioHora = precioHora;
+	}
+	
+	@Override
+	public String toString() {
+		return "[Nombre=" + super.getNombre() + ", edad=" + super.getEdad() + " Cantidad Horas: " + this.cantHoras + " Precio Hora: " + this.precioHora + "]";
+	}
+	public float calcularPago() {
+		return precioHora * cantHoras;
+	}
+		
 
+	
 }
