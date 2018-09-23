5cdeee846647e3667474a8d8a5907ae62735fc7b
David S Gonzalez <davidsghz@gmail.com>
2018-09-23 19:08:57 -0300

    Ejercicio 5 - Opcional

--- 1-2B-TP2/src/ar/edu/ort/taller1/tp2/Ejercicio5/Test.java
+++ 1-2B-TP2/src/ar/edu/ort/taller1/tp2/Ejercicio5/Test.java
@@ -3,7 +3,8 @@ package ar.edu.ort.taller1.tp2.Ejercicio5;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		/*Este ejercicio es opcional,*/
 
 	}
 
